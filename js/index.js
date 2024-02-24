67839e7fbd1c73cdb577ed5bc29e33351e45c3c6
Duc Anh <bully0707ducanh@gmail.com>
2024-02-24 14:01:35 +0700

    changed

--- js/index.js
+++ js/index.js
@@ -1,23 +1,38 @@
 const dictionary = {
     Bungarus_fasciatus:{
         length:["2 mét","2m","2 meters","khoảng 2 mét"],
-        color:["vàng sọc đen","vàng kèm đen","vàng","kem"]
+        color:["vàng sọc đen","vàng kèm đen","vàng","kem"],
+        link:"Bungarus_fasciatus.html",
+        img:"img/Bungarus_fasciatus.jpg",
+        name:"Bungarus fasciatus"
     }
 }
 function find(){
     const inputColor = document.getElementById("color")
     const lengthInput = document.getElementById("length")
     const div = document.getElementById("result")
+    const div2 = document.getElementById("removedDictionary")
+    const dict = document.createElement("div")
+    dict.setAttribute("class","dict")
+    const dict_but = document.createElement("a")
+    dict_but.setAttribute("class","dict_but")
+    const image = document.createElement("img")
+    image.setAttribute("class","image")
+    const overlay = document.createElement("div")
+    overlay.setAttribute("class","overlay")
+    const text = document.createElement("div")
+    text.setAttribute("class","text")
     div.innerHTML="Kết quả tìm kiếm:"
-    if (inputColor.value.toLowerCase()=="vàng".toLowerCase() && lengthInput.value.toLowerCase()=="2m"){
-        div.innerHTML="Kết quả tìm kiếm:"
-        var down = document.createElement("br")
-        var find = document.createElement("a")
-        find.setAttribute("href","Bungarus_fasciatus.html" )
-        var textNode = document.createTextNode("lmao")
-        find.appendChild(textNode)
-        div.appendChild(down)
-        div.appendChild(find)
-
+    dict.appendChild(dict_but)
+    dict_but.appendChild(image)
+    overlay.appendChild(text)
+    dict_but.appendChild(overlay)
+    if (dictionary.Bungarus_fasciatus.color.includes(inputColor.value.toLowerCase()) && dictionary.Bungarus_fasciatus.length.includes(lengthInput.value.toLowerCase())){
+        div.innerHTML="Kết quả tìm kiếm:\n có thể bạn đã gặp phải:"+dictionary.Bungarus_fasciatus.name
+        text.innerHTML=dictionary.Bungarus_fasciatus.name
+        dict_but.setAttribute("href",dictionary.Bungarus_fasciatus.link)
+        image.setAttribute("src",dictionary.Bungarus_fasciatus.img)
+        div2.innerHTML=""
+        div2.appendChild(dict)
     }
 }
